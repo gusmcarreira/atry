76ca3f9c5c2dadf6be58404ae4b7db43ca32e418
gusmcarreira <90475975+gusmcarreira@users.noreply.github.com>
2022-05-05 05:43:12 +0100

    Fixed Bugs Upon Merge

--- src/app/chatbot/chat-buttons/chat-buttons.component.ts
+++ src/app/chatbot/chat-buttons/chat-buttons.component.ts
@@ -1,22 +1,47 @@
-import { Component, Input, Output, EventEmitter, ViewChildren, ElementRef } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { ChatbotService } from '../chatbot.service';
 
 @Component({
   selector: 'chat-buttons',
   template: `
-    <button (click)="onClick()">
+    <button [title]="this.titleBtn" [disabled]="this.clicked" [class.disabled]='this.clicked === true' [class.enabled]='this.clicked === false' (click)="onClick()">
       {{btnLabel}}
     </button>
   `,
   styleUrls: ['./chat-buttons.component.css']
 })
+
 export class ChatButtonsComponent {
-  @Input() public btnLabel: string
-  @Input() public btnPayload: string
+  @Input() public btnLabel: string;
+  @Input() public titleBtn: string;
+  @Input() public btnPayload: string;
+  @Input() public disabled: boolean;
+  @Input() public id;
   @Output() public send = new EventEmitter()
+  public clicked = false;
+
+  constructor(private chatbotService: ChatbotService) {
+    this.chatbotService.enableButton.subscribe(() => {
+      if (this.id === this.chatbotService.buttonToEnable) {
+        this.clicked = false;
+      }
+    });
+    this.chatbotService.disableButton.subscribe(() => {
+      if (this.id === this.chatbotService.buttonToDisable) {
+        this.clicked = true;
+      }
+    });
+  }
 
   onClick() {
-    const message = this.btnPayload
-    const title = this.btnLabel
-    this.send.emit({ message, title })
+    const title = this.btnLabel;
+    if (this.btnPayload !== undefined) {
+      const message = this.btnPayload
+      this.send.emit({ message, title })
+    }
+    else {
+      this.clicked = true;
+      this.chatbotService.chooseConcept(this.id, title);
+    }
   }
 }
